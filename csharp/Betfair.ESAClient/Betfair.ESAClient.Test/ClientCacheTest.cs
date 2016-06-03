e2071dd20b1c91fe2222e12c1dd4dd3615123a9c
mulveyj <james.mulvey@betfair.com>
2016-06-03 17:46:27 +0100

    Added user story test

--- csharp/Betfair.ESAClient/Betfair.ESAClient.Test/ClientCacheTest.cs
+++ csharp/Betfair.ESAClient/Betfair.ESAClient.Test/ClientCacheTest.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using static Betfair.ESASwagger.Model.MarketDataFilter;
 
 namespace Betfair.ESAClient.Test
 {
